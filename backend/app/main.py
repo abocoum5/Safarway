9fcfff10193a675c000106953ad4163eb1b36bf3
abocoum5 <amana.bocoum@email.com>
2026-04-24 20:21:18 +0200

    Fix CORS DELETE

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,9 +14,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://safarway-roan.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
